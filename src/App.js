b58ad4af17c3cf4a51b518b11dd3b9736a935d48
Umeed Negmatullayev <umeednegmatullayev@baggyeongsigs-MacBook-Pro.local>
2019-04-08 01:49:53 +0900

    failed test fixed

--- src/App.js
+++ src/App.js
@@ -56,7 +56,7 @@ class App extends React.Component {
                     type="submit"
                     disabled={submitDisabled}
                   >
-                    Add items
+                    Add item
                   </button>
                 </form>
               </th>
