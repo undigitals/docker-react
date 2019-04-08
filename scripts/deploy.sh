4e5d16d54f23a760e9db421bc493bcc732a2e51b
Umeed Negmatullayev <umeednegmatullayev@baggyeongsigs-MacBook-Pro.local>
2019-04-08 18:18:19 +0900

    script extension changed

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -29,23 +29,3 @@ conn
     )
   });
 
-// example output:
-// Client :: ready
-// STDOUT: Last login: Sun Jun 15 09:37:21 2014 from 192.168.100.100
-//
-// STDOUT: ls -l
-// exit
-//
-// STDOUT: frylock@athf:~$ ls -l
-//
-// STDOUT: total 8
-//
-// STDOUT: drwxr-xr-x 2 frylock frylock 4096 Nov 18  2012 mydir
-//
-// STDOUT: -rw-r--r-- 1 frylock frylock   25 Apr 11  2013 test.txt
-//
-// STDOUT: frylock@athf:~$ exit
-//
-// STDOUT: logout
-//
-// Stream :: close
